735b4f51287b36e5588728c4f0d1ca9baacd3370
Krishna <itzmekrishna.257@gmail.com>
2025-10-20 13:39:47 +0530

    minor error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Navbar from './sections/Navbar.jsx';
+import Projects from './Projects.jsx';
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
